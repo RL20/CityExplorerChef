11c850976b4a6ed232e57fdfb21104fc6540a0ec
harel <harel_shalom@yahoo.com>
2023-06-23 03:38:04 +0300

    Building cookbook infrastructure by ->'chef generate cookbook city_explorer_app_db' command

---
+++ city_explorer_app_db/README.md
@@ -0,0 +1,4 @@
+# city_explorer_app_db
+
+TODO: Enter the cookbook description here.
+
---
+++ city_explorer_app_db/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: city_explorer_app_db
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
