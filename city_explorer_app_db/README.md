6b5bd822bbfac7bf5daa656396c2e38f1fce09d9
harel <harel_shalom@yahoo.com>
2023-06-23 04:17:08 +0300

    add solo.rb configuration file

--- city_explorer_app_db/README.md
+++
@@ -1,4 +0,0 @@
-# city_explorer_app_db
-
-TODO: Enter the cookbook description here.
-
---
+++ solo.rb
@@ -0,0 +1,8 @@
+chef_license 'accept'
+
+# current_dir         = File.expand_path(File.dirname(__FILE__))
+current_dir     = __dir__
+file_cache_path     "#{current_dir}"
+cookbook_path       "#{current_dir}/cookbooks"
+role_path           "#{current_dir}/roles"
+data_bag_path       "#{current_dir}/data_bags
