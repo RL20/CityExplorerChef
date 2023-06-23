0e2510ca23904f41b8901ba2f3243c1882f2a435
harel <harel_shalom@yahoo.com>
2023-06-23 16:07:42 +0300

    insert recipes to default.rb

--- cookbooks/city_explorer_app/recipes/default.rb
+++ cookbooks/city_explorer_app/recipes/default.rb
@@ -1,5 +1,30 @@
 #
-# Cookbook:: city_explorer_app_db
+# Cookbook:: city_explorer_app
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
+
+# Clone the application repository
+git 'https://github.com/RL20/CityExplorer.git' do
+  destination '/opt/cityexplorer'
+  action :sync
+end
+ 
+# Install requirements
+execute 'install_requirements' do
+  command 'pip install -r /opt/cityexplorer/requirements.txt'
+end
+ 
+# Create service
+template '/etc/systemd/system/cityexplorer.service' do
+  source 'cityexplorer.service.erb'
+  variables(
+    app_path: '/opt/cityexplorer',
+    app_port: '8000'
+  )
+end
+ 
+# Start service
+service 'cityexplorer' do
+  action [:enable, :start]
+end
